60cd05871dca914a4da0cc45ea6b5bf5189eb3f4
Shirin <shirin.mansori@gmail.com>
2021-11-09 22:21:09 -0500

    add detail

--- ToDoListAssign4/RootTableViewController.swift
+++ ToDoListAssign4/RootTableViewController.swift
@@ -6,27 +6,34 @@
 //
 
 import UIKit
+import Foundation
 
 class RootTableViewController: UITableViewController {
-    private var myList: [String : Date] = [:]
+    private var myList: [String : [Date : Int]] = [:]
     private var cellPointSize: CGFloat!
     private var toDoList: ToDoList!
-    private static let familyCell = "Cell"
-    private static let favoritesCell = "Favorites"
+    private static let TaskCell = "Cell"
+    private static let DetailCell = "CellDetail"
     override func viewDidLoad() {
         super.viewDidLoad()
 
  
         tableView.reloadData()
 
+        toDoList = ToDoList()
+   
+        toDoList.addToList(newItemName: "MyFirstTask", dueDate: Date() , status: 0)
+        toDoList.addToList(newItemName: "MySecondTask", dueDate: Date.tomorrow , status: 1)
+        toDoList.addToList(newItemName: "MyThirdTask", dueDate: Date.dayAfterTomorrow  , status: 1)
+        toDoList.addToList(newItemName: "MyForthTask", dueDate: Date.yesterday  , status: 1)
+        toDoList.addToList(newItemName: "MyFifthTask", dueDate:Date.dayBeforeYesterday , status: 1)
+        toDoList.addToList(newItemName: "MySixthTask", dueDate: Date()  , status: 0)
+        toDoList.addToList(newItemName: "MySeventhTask", dueDate: Date.tomorrow  , status: 1)
         
-        myList.updateValue(Date(), forKey: "MyFirstTask")
-     myList.updateValue(Date.tomorrow, forKey: "MySecondTask")
-         myList.updateValue(Date.dayAfterTomorrow, forKey: "MyThirdTask")
-        myList.updateValue(Date.yesterday, forKey: "MyForthTask")
         let preferredTableViewFont = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
         cellPointSize = preferredTableViewFont.pointSize
         tableView.estimatedRowHeight = cellPointSize
+        myList  = toDoList.myToDoList
    
     }
 
@@ -43,15 +50,45 @@ class RootTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
                     // The to do list names
-                    let cell = tableView.dequeueReusableCell(withIdentifier: RootTableViewController.familyCell, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: RootTableViewController.TaskCell, for: indexPath)
+
                     cell.textLabel?.font = UIFont(name:"FontAwesome",size:15)
                     let index = myList.index(myList.startIndex , offsetBy: indexPath.row)
                     cell.textLabel?.text = myList.keys[index]
-                    cell.detailTextLabel?.text =  myList.keys[index]
+                    cell.detailTextLabel?.text =  getTaskStatus(taskKey: myList.keys[index])
+                    
              return cell
              
        
         
+    }
+    private func getTaskStatus(taskKey : String) -> String
+    {
+        let result : [Date : Int]? = myList[taskKey]
+        for item in result!.keys {
+            if (result?[item] == 0)
+            {
+                return "Completed"
+                
+            }
+            else
+            {
+                if (item < Date())
+                {
+                    return "OverDue"
+                }
+                else
+                {
+                    let dateString = "\(item)"
+                    let index = dateString.index(dateString.startIndex, offsetBy: 16)
+                    return   String(dateString.prefix(upTo: index))
+                }
+               
+                
+            }
+          
+        }
+        return ""
     }
  
     /*
--- ToDoListAssign4/ToDoList.swift
+++ ToDoListAssign4/ToDoList.swift
@@ -10,18 +10,18 @@ import UIKit
 class ToDoList
 {
     static let sharedToDoList = ToDoList()
-    private(set) var myToDoList: [String: Date] = [:]
+    private(set) var myToDoList: [String: [Date : Int]] = [:]
     init()
     {
         let defaults = UserDefaults.standard
-        let storedList = defaults.object(forKey: "ToDoList") as? [String:Date]
+        let storedList = defaults.object(forKey: "ToDoList") as? [String:[Date : Int]]
         myToDoList = storedList != nil ? storedList! : [:]
         
     }
-    func addToList(newItemName: String , dueDate : Date) {
+    func addToList(newItemName: String , dueDate : Date , status : Int) {
         if !myToDoList.keys.contains(newItemName) {
-            myToDoList.updateValue(dueDate, forKey: newItemName)
-            saveList()
+            myToDoList.updateValue([dueDate : status], forKey: newItemName)
+         
             }
         }
         
